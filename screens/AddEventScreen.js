afc095d06b75dd3f5cdf5eee28a5e758487f161b
stacy leitstein <unique523@yahoo.com>
2022-03-30 00:03:47 -0400

    updated add event

--- screens/AddEventScreen.js
+++ screens/AddEventScreen.js
@@ -1,24 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import { Button } from 'react-native-paper'
-import React from 'react'
+import React, { useState } from 'react'
+import Background from '../components/Background'
+
 
 const AddEvent = () => {
+  const [name, setName] = useState('')
+  const [location, setLocation] = useState('')
+  const [description, setDescription] = useState('')
+  const [cost, setCost] = useState('')
+  const [comment, setComment] = useState('')
   return (
-    <View>
-      <Text>Add Event</Text>
-      <Text>Name of the Event:</Text>
-      {/* <Image>Event photo</Image> */}
-      <Text> Details of the Event:</Text>
-      <Text>Location:</Text>
-      <Text>Price</Text>
-      <Text>Comment or tips</Text>
-      <Button onPress={() => navigation.navigate('ContactScreen')}>Go to Contact Us</Button>
-      <Button onPress={() => navigation.navigate('HomeScreen')}>Home</Button>
-    </View>
+    <Background>
+      <View style={styles.eventContainer}>
+        <Text>Add Event</Text>
+        <Text>Name of the Event:</Text>
+        <TextInput
+          placeholder='Add Event Name'
+          value={name}
+          onChange={setName}
+          style={styles.inputStyles}
+        />
+        {/* <Image>Event photo</Image> */}
+        <Text> Details of the Event:</Text>
+        <TextInput
+          placeholder='Add Description'
+          value={description}
+          onChange={setDescription}
+          style={styles.inputStyles}
+        />
+        <Text>Location:</Text>
+        <TextInput
+          placeholder='Location'
+          value={location}
+          onChange={setLocation}
+          style={styles.inputStyles}
+        />
+        <Text>Price</Text>
+        <TextInput
+          placeholder='Price'
+          value={cost}
+          onChange={setCost}
+          style={styles.inputStyles}
+        />
+        <Text>Comment or tips</Text>
+        <TextInput
+          placeholder='Add Description'
+          value={comment}
+          onChange={setComment}
+          style={styles.inputStyles}
+        />
+       
+        <Button onPress={() => navigation.navigate('HomeScreen')}>Home</Button>
+      </View>
+    </Background>
   )
 }
 // add ability to upload an Event
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  eventContainer:{
+    justifyContent: 'center',
+    alignSelf: 'center'
+  },
+  inputStyles: {
+    borderColor: 'black',
+    borderRadius: 10,
+    borderWidth: 1
+  }
+})
 
 export default AddEvent
